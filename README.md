ddc1d70fe081434bc7312d6cce5127ba7f48678c
Artem Chu <vip.chuprakov.artem@mail.ru>
2024-01-17 13:59:50 +0300

    stepper motor control via driver

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # RobotMotorsControl
 Training in working with Git, basic => controlling the movement of robot motors.
+
+StepperMotor-28BYJ-48
+
+Usage
+sudo pip3 install gpiozero
+
+python3 StepperMotor.py
+
+References
+https://www.raspberrypi.com/documentation/computers/os.html
+https://learn.adafruit.com/adafruits-raspberry-pi-lesson-10-stepper-motors/software
+https://st-dvorik.ru/uchenikam/python/
+
---
+++ motor_control.py
@@ -0,0 +1,48 @@
+from gpiozero import LED
+import time
+
+class StepperMotor:
+
+    def __init__(self, A1, A2, B1, B2):
+        self.PIN_A1 = LED(A1)
+        self.PIN_A2 = LED(A2)
+        self.PIN_B1 = LED(B1)
+        self.PIN_B2 = LED(B2)
+
+    def forward(self, delay, steps):
+        for i in range(steps):
+            self.setStep(1, 0, 1, 0)
+            time.sleep(delay)
+            self.setStep(0, 1, 1, 0)
+            time.sleep(delay)
+            self.setStep(0, 1, 0, 1)
+            time.sleep(delay)
+            self.setStep(1, 0, 0, 1)
+            time.sleep(delay)
+
+    def backward(self, delay, steps):
+        for i in range(steps):
+            self.setStep(1, 0, 0, 1)
+            time.sleep(delay)
+            self.setStep(0, 1, 0, 1)
+            time.sleep(delay)
+            self.setStep(0, 1, 1, 0)
+            time.sleep(delay)
+            self.setStep(1, 0, 1, 0)
+            time.sleep(delay)
+
+    def setStep(self, w1, w2, w3, w4):
+        self.PIN_A1.value = w1
+        self.PIN_A2.value = w2
+        self.PIN_B1.value = w3
+        self.PIN_B2.value = w4
+
+if __name__ == '__main__':
+    from signal import pause
+
+    motor1 = StepperMotor(A1=22, A2=27, B1=17, B2=18)
+
+    while True:
+        motor1.forward(delay=5.0/1000, steps=512)
+        time.sleep(1)
+        pause()
